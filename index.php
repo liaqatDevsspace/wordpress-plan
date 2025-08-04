67f6df0751e47bde77cf44952947df78882f6a85
Ahmad <ahmad.raza@devsspace.com>
2025-08-04 14:53:52 +0500

    test-commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>PHP</title>
+</head>
+
+<body>
+  <!-- Variables -->
+  <?php
+  $name = "Liaqat Ali";
+  echo "Hi $name, welcome to PHP!";
+  ?>
+</body>
+
+</html>
