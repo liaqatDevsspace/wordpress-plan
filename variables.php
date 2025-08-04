7fc8b8c66726167b546655134a8f6f4d582f375d
Liaqat Ali <liaqat.ali@devsspace.com>
2025-08-04 15:32:28 +0500

    created some variables

---
+++ variables.php
@@ -0,0 +1,19 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+
+<body>
+    <!-- Variables -->
+    <?php
+    $name = "Liaqat Ali";
+    $age = 25;
+    $isDeveloper = true;
+    echo "Hi my name is $name, I am $age years old and it is " . ($isDeveloper ? "true" : "false") . " that I am a developer.";
+    ?>
+</body>
+
+</html>
